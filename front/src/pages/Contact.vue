15f835d10be7445bf54b704f73a343501872a558
Tuomas <tuomas.j.kontola@aalto.fi>
2019-02-17 19:43:50 +0200

    Adds some db stuff

---
+++ front/src/pages/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p v-text="$t('home.welcome_msg')"></p>
+  </div>
+</template>
+
+<script>
+  /* ============
+   * Home Index Page
+   * ============
+   *
+   * The home index page.
+   */
+export default {
+  /**
+   * The name of the page.
+   */
+  name: 'contact-index',
+
+  /**
+   * The components that the page can use.
+   */
+  components: {
+  },
+};
+</script>
