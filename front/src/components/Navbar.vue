fbde5056ecc822bbdd504078f719c50fae03a312
Tuomas <tuomas.j.kontola@aalto.fi>
2019-02-25 23:59:03 +0200

    Navbar

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,38 +1,10 @@
 <template>
-  <nav class="navbar transparent" role="navigation" aria-label="main navigation">
-    <div class="container">
-
-    <div class="navbar-brand transparent">
-      <a role="button"
-          class="navbar-burger burger"
-          :class="{ 'is-active': showNavbar }"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navigationBar"
-          @click="toggleNavbar">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-    <div id="navigationBar"
-    class="navbar-menu transparent"
-    :class="{ 'is-active': showNavbar }">
-      <div class="navbar-start">
-        <router-link class="navbar-item" v-text="$t('navigation.frontpage')" to="/"></router-link>
-        <router-link class="navbar-item" v-text="$t('navigation.register')" to="/signup"></router-link>
-        <router-link class="navbar-item" v-text="$t('navigation.week')" to="/participants"></router-link>
-        <router-link class="navbar-item" v-text="$t('navigation.companies')" to="/"></router-link>
-        <router-link class="navbar-item" v-text="$t('navigation.contact')" to="/contact"></router-link>
-      </div>
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <v-locale></v-locale>
-        </div>
-      </div>
-    </div>
-  </div>
-
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <router-link class="navbar__item" v-text="$t('navigation.frontpage')" to="/"></router-link>
+    <router-link class="navbar__item" v-text="$t('navigation.register')" to="/signup"></router-link>
+    <router-link class="navbar__item" v-text="$t('navigation.week')" to="/participants"></router-link>
+    <router-link class="navbar__item" v-text="$t('navigation.companies')" to="/"></router-link>
+    <router-link class="navbar__item" v-text="$t('navigation.contact')" to="/contact"></router-link>
   </nav>
 </template>
 
@@ -42,7 +14,7 @@ export default {
   name: 'Navbar',
   data() {
     return {
-      showNavbar: false,
+      showNavbar: true,
     };
   },
   methods: {
