72df619d8a1a170975529b62975c882b068826c8
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2019-09-18 15:03:53 +0300

    asd

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 export default {
-    signupStartInvited: new Date('2019-09-25'),
-    signupStart: new Date('2019-09-9'),
+    signupStartInvited: new Date('2018-09-25'),
+    signupStart: new Date('2018-09-9'),
     signupEnd: new Date('2020'),
 }
