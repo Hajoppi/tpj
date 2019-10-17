4153571989c33b5a121e32acc6c8f4f36143523c
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2019-10-17 19:01:43 +0300

    updates max participants

--- backend/app/config.js
+++ backend/app/config.js
@@ -3,5 +3,5 @@ module.exports = {
     signupEndInvited: new Date(1570568399000),
     signupStart: new Date(1570611600000),
     signupEnd: new Date(1571950799000),
-    maxParticipants: 300,
+    maxParticipants: 324,
 }
