563b4ab4ad8c0aa3524521b17f82b224978c2057
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2019-11-20 16:50:34 +0200

    removes redundant file

--- backend/services/asdf.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto'),
-      algorithm = 'aes256';
-const cipher = crypto.createCipher(algorithm, 'teekkkariusOnTodella123Tyolasta567!KaikkiToimii');
-let crypted = cipher.update('146', 'utf8', 'hex');
-crypted += cipher.final('hex');
-console.log("https://teekkarius147.ayy.fi/#/edit?id="+crypted);
